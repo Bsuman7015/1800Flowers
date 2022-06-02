6c091a601ab5c7213d20a82d9b7f105d6bb695b5
Bsuman7015 <101004327+Bsuman7015@users.noreply.github.com>
2022-06-02 16:39:04 +0530

    Add files via upload

---
+++ Spring1800Flowers/src/main/java/com/flowers/Spring1800FlowersApplication.java
@@ -0,0 +1,42 @@
+package com.flowers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.flowers.service.AccountService;
+
+@SpringBootApplication
+@ComponentScan({"com.flowers.*"})
+public class Spring1800FlowersApplication {
+	
+	@Autowired
+	AccountService accountService;
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1800FlowersApplication.class, args);
+		
+	}
+	
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+	    ApplicationContext applicationContext = event.getApplicationContext();
+	    RequestMappingHandlerMapping requestMappingHandlerMapping = applicationContext
+	        .getBean("requestMappingHandlerMapping", RequestMappingHandlerMapping.class);
+	    Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping
+	        .getHandlerMethods();
+	  //  map.forEach((key, value) -> System.out.println( key+" "+value));
+	    accountService.getEnPointCount(map);
+	}
+	
+
+}
